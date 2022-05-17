f74f0368ba40ca02255ef1f43381db2cdde25153
Matthew Hutter <matthewjhutter@gmail.com>
2022-05-17 16:16:09 -0700

    06 solved

--- practice-for-week-02-pojo-main/problems/06-key-in-object-array.js
+++ practice-for-week-02-pojo-main/problems/06-key-in-object-array.js
@@ -1,7 +1,7 @@
 /***********************************************************************
-Write a function `keyInObjectArray(objArray, keyString)` that takes in an array of 
-objects as the first parameter and a string as the second. The `keyInObjectArray` 
-will return `true` if any of the objects contains the `keyString` as a key within them, and 
+Write a function `keyInObjectArray(objArray, keyString)` that takes in an array of
+objects as the first parameter and a string as the second. The `keyInObjectArray`
+will return `true` if any of the objects contains the `keyString` as a key within them, and
 `false` if not.
 
 
@@ -21,7 +21,26 @@ keyInObjectArray(objArray, 'animal'); // => false
 
 function keyInObjectArray(objArray, keyString) {
   // Your code here
+  for (let i = 0; i < objArray.length; i++){
+    let obj = objArray[i];
+    for (let key in obj){
+      if (key === keyString ){
+        return true;
+     }
+    }
+  }
+  return false;
 }
 
+let objArray = [
+  { name: "Rupert" },
+  { age: 42 },
+  { planet: "Earth", system: "Milky Way" }
+];
+
+keyInObjectArray(objArray, 'planet'); // => true
+keyInObjectArray(objArray, 'age'); // => true
+keyInObjectArray(objArray, 'food'); // => false
+keyInObjectArray(objArray, 'animal'); // => false
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyInObjectArray;
