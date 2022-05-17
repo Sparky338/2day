7de56ac0aab5b2e5911423bc522b5d84f1a383ee
Matthew Hutter <matthewjhutter@gmail.com>
2022-05-17 12:42:44 -0700

    initial commit

---
+++ my-object-practice.js
@@ -0,0 +1,21 @@
+let myObj = {};
+
+myObj["favFood"] = "pizza";
+myObj["favNumber"] = 13;
+myObj["birthday"] = "4/20/1989";
+
+
+
+console.log(myObj)
+
+
+let show = {
+    name: "Star Trek",
+    series: ["TOS", "TNG", "DS9", "VOY"],
+    captains: {
+      TOS: "Kirk",
+      TNG: "Picard",
+      DS9: "Sisko",
+      VOY: "Janeway"
+    },
+  }
