a5f28292cbf22cf0a9495ed73ff59450278e6fc6
Matthew Hutter <matthewjhutter@gmail.com>
2022-05-17 13:28:37 -0700

    02 solved

--- practice-for-week-02-pojo-main/problems/01-keys-in-object.js
+++ practice-for-week-02-pojo-main/problems/01-keys-in-object.js
@@ -23,9 +23,5 @@ function keysInObject(obj) {
 //   return(keyArr);
 // }
 
-let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
-let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
-keysInObject(animals); // => ["dog", "cat", "bison"]
-keysInObject(foods); // => ["apple", "lemon", "mango"]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keysInObject;
--- practice-for-week-02-pojo-main/problems/02-values-in-object.js
+++ practice-for-week-02-pojo-main/problems/02-values-in-object.js
@@ -1,6 +1,6 @@
 /***********************************************************************
-Write a function called `valuesInObject(obj)` that takes in an object and returns 
-an array of all the values within that Object. 
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
 
 
 Do this once using using a `for...in` loop and once using `Object.values`.
@@ -16,7 +16,18 @@ valuesInObject(foods); // => ["tart", "sour", "sweet"]
 
 function valuesInObject(obj) {
   // Your code here
+  let valueArr = [];
+
+  for (let key in obj){
+    let values = obj[key];
+    valueArr.push(values);
+  }
+  return valueArr;
 }
 
+let animals = {dog: "Wolfie", cat: "Jet", bison: "Bilbo"}
+let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
+valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
+valuesInObject(foods); // => ["tart", "sour", "sweet"]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuesInObject;
